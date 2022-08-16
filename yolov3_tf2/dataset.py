098d48930739858b18972829c0698651abf13c4d
Prashant Patel <prp0x80@gmail.com>
2022-08-16 14:13:14 +0000

    Core updates

--- yolov3_tf2/dataset.py
+++ yolov3_tf2/dataset.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
-from absl.flags import FLAGS
+
+from .utils import float_feature, int_feature, string_feature
+
 
 @tf.function
 def transform_targets_for_output(y_true, grid_size, anchor_idxs):
@@ -8,7 +10,8 @@ def transform_targets_for_output(y_true, grid_size, anchor_idxs):
 
     # y_true_out: (N, grid, grid, anchors, [x1, y1, x2, y2, obj, class])
     y_true_out = tf.zeros(
-        (N, grid_size, grid_size, tf.shape(anchor_idxs)[0], 6))
+        (N, grid_size, grid_size, tf.shape(anchor_idxs)[0], 6)
+    )
 
     anchor_idxs = tf.cast(anchor_idxs, tf.int32)
 
@@ -20,27 +23,28 @@ def transform_targets_for_output(y_true, grid_size, anchor_idxs):
             if tf.equal(y_true[i][j][2], 0):
                 continue
             anchor_eq = tf.equal(
-                anchor_idxs, tf.cast(y_true[i][j][5], tf.int32))
+                anchor_idxs, tf.cast(y_true[i][j][5], tf.int32)
+            )
 
             if tf.reduce_any(anchor_eq):
                 box = y_true[i][j][0:4]
                 box_xy = (y_true[i][j][0:2] + y_true[i][j][2:4]) / 2
 
                 anchor_idx = tf.cast(tf.where(anchor_eq), tf.int32)
-                grid_xy = tf.cast(box_xy // (1/grid_size), tf.int32)
+                grid_xy = tf.cast(box_xy // (1 / grid_size), tf.int32)
 
                 # grid[y][x][anchor] = (tx, ty, bw, bh, obj, class)
                 indexes = indexes.write(
-                    idx, [i, grid_xy[1], grid_xy[0], anchor_idx[0][0]])
+                    idx, [i, grid_xy[1], grid_xy[0], anchor_idx[0][0]]
+                )
                 updates = updates.write(
-                    idx, [box[0], box[1], box[2], box[3], 1, y_true[i][j][4]])
+                    idx, [box[0], box[1], box[2], box[3], 1, y_true[i][j][4]]
+                )
                 idx += 1
 
-    # tf.print(indexes.stack())
-    # tf.print(updates.stack())
-
     return tf.tensor_scatter_nd_update(
-        y_true_out, indexes.stack(), updates.stack())
+        y_true_out, indexes.stack(), updates.stack()
+    )
 
 
 def transform_targets(y_train, anchors, anchor_masks, size):
@@ -51,11 +55,13 @@ def transform_targets(y_train, anchors, anchor_masks, size):
     anchors = tf.cast(anchors, tf.float32)
     anchor_area = anchors[..., 0] * anchors[..., 1]
     box_wh = y_train[..., 2:4] - y_train[..., 0:2]
-    box_wh = tf.tile(tf.expand_dims(box_wh, -2),
-                     (1, 1, tf.shape(anchors)[0], 1))
+    box_wh = tf.tile(
+        tf.expand_dims(box_wh, -2), (1, 1, tf.shape(anchors)[0], 1)
+    )
     box_area = box_wh[..., 0] * box_wh[..., 1]
-    intersection = tf.minimum(box_wh[..., 0], anchors[..., 0]) * \
-        tf.minimum(box_wh[..., 1], anchors[..., 1])
+    intersection = tf.minimum(box_wh[..., 0], anchors[..., 0]) * tf.minimum(
+        box_wh[..., 1], anchors[..., 1]
+    )
     iou = intersection / (box_area + anchor_area - intersection)
     anchor_idx = tf.cast(tf.argmax(iou, axis=-1), tf.float32)
     anchor_idx = tf.expand_dims(anchor_idx, axis=-1)
@@ -63,8 +69,9 @@ def transform_targets(y_train, anchors, anchor_masks, size):
     y_train = tf.concat([y_train, anchor_idx], axis=-1)
 
     for anchor_idxs in anchor_masks:
-        y_outs.append(transform_targets_for_output(
-            y_train, grid_size, anchor_idxs))
+        y_outs.append(
+            transform_targets_for_output(y_train, grid_size, anchor_idxs)
+        )
         grid_size *= 2
 
     return tuple(y_outs)
@@ -76,69 +83,40 @@ def transform_images(x_train, size):
     return x_train
 
 
-# https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/using_your_own_dataset.md#conversion-script-outline-conversion-script-outline
-# Commented out fields are not required in our project
-IMAGE_FEATURE_MAP = {
-    # 'image/width': tf.io.FixedLenFeature([], tf.int64),
-    # 'image/height': tf.io.FixedLenFeature([], tf.int64),
-    # 'image/filename': tf.io.FixedLenFeature([], tf.string),
-    # 'image/source_id': tf.io.FixedLenFeature([], tf.string),
-    # 'image/key/sha256': tf.io.FixedLenFeature([], tf.string),
-    'image/encoded': tf.io.FixedLenFeature([], tf.string),
-    # 'image/format': tf.io.FixedLenFeature([], tf.string),
-    'image/object/bbox/xmin': tf.io.VarLenFeature(tf.float32),
-    'image/object/bbox/ymin': tf.io.VarLenFeature(tf.float32),
-    'image/object/bbox/xmax': tf.io.VarLenFeature(tf.float32),
-    'image/object/bbox/ymax': tf.io.VarLenFeature(tf.float32),
-    'image/object/class/text': tf.io.VarLenFeature(tf.string),
-    # 'image/object/class/label': tf.io.VarLenFeature(tf.int64),
-    # 'image/object/difficult': tf.io.VarLenFeature(tf.int64),
-    # 'image/object/truncated': tf.io.VarLenFeature(tf.int64),
-    # 'image/object/view': tf.io.VarLenFeature(tf.string),
+PARSING_CONFIG = {
+    "image/filename": string_feature(),
+    "image/encoded": string_feature(),
+    "image/width": int_feature(),
+    "image/height": int_feature(),
+    "image/bbox/xmin": float_feature(fixed_len=False),
+    "image/bbox/ymin": float_feature(fixed_len=False),
+    "image/bbox/xmax": float_feature(fixed_len=False),
+    "image/bbox/ymax": float_feature(fixed_len=False),
+    "image/bbox/labels": int_feature(fixed_len=False),
 }
 
 
-def parse_tfrecord(tfrecord, class_table, size):
-    x = tf.io.parse_single_example(tfrecord, IMAGE_FEATURE_MAP)
-    x_train = tf.image.decode_jpeg(x['image/encoded'], channels=3)
-    x_train = tf.image.resize(x_train, (size, size))
-
-    class_text = tf.sparse.to_dense(
-        x['image/object/class/text'], default_value='')
-    labels = tf.cast(class_table.lookup(class_text), tf.float32)
-    y_train = tf.stack([tf.sparse.to_dense(x['image/object/bbox/xmin']),
-                        tf.sparse.to_dense(x['image/object/bbox/ymin']),
-                        tf.sparse.to_dense(x['image/object/bbox/xmax']),
-                        tf.sparse.to_dense(x['image/object/bbox/ymax']),
-                        labels], axis=1)
-
-    paddings = [[0, FLAGS.yolo_max_boxes - tf.shape(y_train)[0]], [0, 0]]
+def parse_tfrecord(tfrecord):
+    x = tf.io.parse_single_example(tfrecord, PARSING_CONFIG)
+    x_train = tf.image.decode_png(x["image/encoded"], channels=3)
+    labels = tf.sparse.to_dense(x["image/bbox/labels"])
+    y_train = tf.stack(
+        [
+            tf.sparse.to_dense(x["image/bbox/xmin"]),
+            tf.sparse.to_dense(x["image/bbox/ymin"]),
+            tf.sparse.to_dense(x["image/bbox/xmax"]),
+            tf.sparse.to_dense(x["image/bbox/ymax"]),
+            tf.cast(labels, dtype=tf.float32),
+        ],
+        axis=1,
+    )
+    yolo_max_boxes = 100
+    paddings = [[0, yolo_max_boxes - tf.shape(y_train)[0]], [0, 0]]
     y_train = tf.pad(y_train, paddings)
-
     return x_train, y_train
 
 
-def load_tfrecord_dataset(file_pattern, class_file, size=416):
-    LINE_NUMBER = -1  # TODO: use tf.lookup.TextFileIndex.LINE_NUMBER
-    class_table = tf.lookup.StaticHashTable(tf.lookup.TextFileInitializer(
-        class_file, tf.string, 0, tf.int64, LINE_NUMBER, delimiter="\n"), -1)
-
-    files = tf.data.Dataset.list_files(file_pattern)
-    dataset = files.flat_map(tf.data.TFRecordDataset)
-    return dataset.map(lambda x: parse_tfrecord(x, class_table, size))
-
-
-def load_fake_dataset():
-    x_train = tf.image.decode_jpeg(
-        open('./data/girl.png', 'rb').read(), channels=3)
-    x_train = tf.expand_dims(x_train, axis=0)
-
-    labels = [
-        [0.18494931, 0.03049111, 0.9435849,  0.96302897, 0],
-        [0.01586703, 0.35938117, 0.17582396, 0.6069674, 56],
-        [0.09158827, 0.48252046, 0.26967454, 0.6403017, 67]
-    ] + [[0, 0, 0, 0, 0]] * 5
-    y_train = tf.convert_to_tensor(labels, tf.float32)
-    y_train = tf.expand_dims(y_train, axis=0)
-
-    return tf.data.Dataset.from_tensor_slices((x_train, y_train))
+def load_tfrecord_dataset(tfrecords_path: str):
+    dataset = tf.data.TFRecordDataset(tfrecords_path)
+    # dataset = files.flat_map(tf.data.TFRecordDataset)
+    return dataset.map(lambda x: parse_tfrecord(x))
--- yolov3_tf2/utils.py
+++ yolov3_tf2/utils.py
@@ -1,29 +1,36 @@
-from absl import logging
+import logging
+from typing import Union
+
+import cv2
 import numpy as np
 import tensorflow as tf
-import cv2
+from tensorflow.python.ops.parsing_config import (
+    FixedLenFeature,
+    FixedLenSequenceFeature,
+    VarLenFeature,
+)
 
 YOLOV3_LAYER_LIST = [
-    'yolo_darknet',
-    'yolo_conv_0',
-    'yolo_output_0',
-    'yolo_conv_1',
-    'yolo_output_1',
-    'yolo_conv_2',
-    'yolo_output_2',
+    "yolo_darknet",
+    "yolo_conv_0",
+    "yolo_output_0",
+    "yolo_conv_1",
+    "yolo_output_1",
+    "yolo_conv_2",
+    "yolo_output_2",
 ]
 
 YOLOV3_TINY_LAYER_LIST = [
-    'yolo_darknet',
-    'yolo_conv_0',
-    'yolo_output_0',
-    'yolo_conv_1',
-    'yolo_output_1',
+    "yolo_darknet",
+    "yolo_conv_0",
+    "yolo_output_0",
+    "yolo_conv_1",
+    "yolo_output_1",
 ]
 
 
 def load_darknet_weights(model, weights_file, tiny=False):
-    wf = open(weights_file, 'rb')
+    wf = open(weights_file, "rb")
     major, minor, revision, seen, _ = np.fromfile(wf, dtype=np.int32, count=5)
 
     if tiny:
@@ -34,15 +41,19 @@ def load_darknet_weights(model, weights_file, tiny=False):
     for layer_name in layers:
         sub_model = model.get_layer(layer_name)
         for i, layer in enumerate(sub_model.layers):
-            if not layer.name.startswith('conv2d'):
+            if not layer.name.startswith("conv2d"):
                 continue
             batch_norm = None
-            if i + 1 < len(sub_model.layers) and \
-                    sub_model.layers[i + 1].name.startswith('batch_norm'):
+            if i + 1 < len(sub_model.layers) and sub_model.layers[
+                i + 1
+            ].name.startswith("batch_norm"):
                 batch_norm = sub_model.layers[i + 1]
 
-            logging.info("{}/{} {}".format(
-                sub_model.name, layer.name, 'bn' if batch_norm else 'bias'))
+            logging.info(
+                "{}/{} {}".format(
+                    sub_model.name, layer.name, "bn" if batch_norm else "bias"
+                )
+            )
 
             filters = layer.filters
             size = layer.kernel_size[0]
@@ -53,17 +64,20 @@ def load_darknet_weights(model, weights_file, tiny=False):
             else:
                 # darknet [beta, gamma, mean, variance]
                 bn_weights = np.fromfile(
-                    wf, dtype=np.float32, count=4 * filters)
+                    wf, dtype=np.float32, count=4 * filters
+                )
                 # tf [gamma, beta, mean, variance]
                 bn_weights = bn_weights.reshape((4, filters))[[1, 0, 2, 3]]
 
             # darknet shape (out_dim, in_dim, height, width)
             conv_shape = (filters, in_dim, size, size)
             conv_weights = np.fromfile(
-                wf, dtype=np.float32, count=np.product(conv_shape))
+                wf, dtype=np.float32, count=np.product(conv_shape)
+            )
             # tf shape (height, width, in_dim, out_dim)
-            conv_weights = conv_weights.reshape(
-                conv_shape).transpose([2, 3, 1, 0])
+            conv_weights = conv_weights.reshape(conv_shape).transpose(
+                [2, 3, 1, 0]
+            )
 
             if batch_norm is None:
                 layer.set_weights([conv_weights, conv_bias])
@@ -71,7 +85,7 @@ def load_darknet_weights(model, weights_file, tiny=False):
                 layer.set_weights([conv_weights])
                 batch_norm.set_weights(bn_weights)
 
-    assert len(wf.read()) == 0, 'failed to read all data'
+    assert len(wf.read()) == 0, "failed to read all data"
     wf.close()
 
 
@@ -87,29 +101,48 @@ def broadcast_iou(box_1, box_2):
     box_1 = tf.broadcast_to(box_1, new_shape)
     box_2 = tf.broadcast_to(box_2, new_shape)
 
-    int_w = tf.maximum(tf.minimum(box_1[..., 2], box_2[..., 2]) -
-                       tf.maximum(box_1[..., 0], box_2[..., 0]), 0)
-    int_h = tf.maximum(tf.minimum(box_1[..., 3], box_2[..., 3]) -
-                       tf.maximum(box_1[..., 1], box_2[..., 1]), 0)
+    int_w = tf.maximum(
+        tf.minimum(box_1[..., 2], box_2[..., 2])
+        - tf.maximum(box_1[..., 0], box_2[..., 0]),
+        0,
+    )
+    int_h = tf.maximum(
+        tf.minimum(box_1[..., 3], box_2[..., 3])
+        - tf.maximum(box_1[..., 1], box_2[..., 1]),
+        0,
+    )
     int_area = int_w * int_h
-    box_1_area = (box_1[..., 2] - box_1[..., 0]) * \
-        (box_1[..., 3] - box_1[..., 1])
-    box_2_area = (box_2[..., 2] - box_2[..., 0]) * \
-        (box_2[..., 3] - box_2[..., 1])
+    box_1_area = (box_1[..., 2] - box_1[..., 0]) * (
+        box_1[..., 3] - box_1[..., 1]
+    )
+    box_2_area = (box_2[..., 2] - box_2[..., 0]) * (
+        box_2[..., 3] - box_2[..., 1]
+    )
     return int_area / (box_1_area + box_2_area - int_area)
 
 
 def draw_outputs(img, outputs, class_names):
     boxes, objectness, classes, nums = outputs
-    boxes, objectness, classes, nums = boxes[0], objectness[0], classes[0], nums[0]
+    boxes, objectness, classes, nums = (
+        boxes[0],
+        objectness[0],
+        classes[0],
+        nums[0],
+    )
     wh = np.flip(img.shape[0:2])
     for i in range(nums):
         x1y1 = tuple((np.array(boxes[i][0:2]) * wh).astype(np.int32))
         x2y2 = tuple((np.array(boxes[i][2:4]) * wh).astype(np.int32))
         img = cv2.rectangle(img, x1y1, x2y2, (255, 0, 0), 2)
-        img = cv2.putText(img, '{} {:.4f}'.format(
-            class_names[int(classes[i])], objectness[i]),
-            x1y1, cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
+        img = cv2.putText(
+            img,
+            "{} {:.4f}".format(class_names[int(classes[i])], objectness[i]),
+            x1y1,
+            cv2.FONT_HERSHEY_COMPLEX_SMALL,
+            1,
+            (0, 0, 255),
+            2,
+        )
     return img
 
 
@@ -122,9 +155,15 @@ def draw_labels(x, y, class_names):
         x1y1 = tuple((np.array(boxes[i][0:2]) * wh).astype(np.int32))
         x2y2 = tuple((np.array(boxes[i][2:4]) * wh).astype(np.int32))
         img = cv2.rectangle(img, x1y1, x2y2, (255, 0, 0), 2)
-        img = cv2.putText(img, class_names[classes[i]],
-                          x1y1, cv2.FONT_HERSHEY_COMPLEX_SMALL,
-                          1, (0, 0, 255), 2)
+        img = cv2.putText(
+            img,
+            class_names[classes[i]],
+            x1y1,
+            cv2.FONT_HERSHEY_COMPLEX_SMALL,
+            1,
+            (0, 0, 255),
+            2,
+        )
     return img
 
 
@@ -133,3 +172,35 @@ def freeze_all(model, frozen=True):
     if isinstance(model, tf.keras.Model):
         for l in model.layers:
             freeze_all(l, frozen)
+
+
+def int_feature(
+    allow_missing: bool = True,
+    fixed_len: bool = True,
+    dtype: tf.dtypes.DType = tf.int64,
+) -> Union[FixedLenSequenceFeature, VarLenFeature]:
+    if fixed_len:
+        return tf.io.FixedLenSequenceFeature(
+            [], dtype=tf.int64, allow_missing=allow_missing
+        )
+    return tf.io.VarLenFeature(dtype=dtype)
+
+
+def float_feature(
+    allow_missing: bool = True,
+    fixed_len: bool = True,
+    dtype: tf.dtypes.DType = tf.float32,
+) -> Union[FixedLenSequenceFeature, VarLenFeature]:
+    if fixed_len:
+        return tf.io.FixedLenSequenceFeature(
+            [], dtype=tf.float32, allow_missing=allow_missing
+        )
+    return tf.io.VarLenFeature(dtype=dtype)
+
+
+def string_feature(
+    fixed_len: bool = True,
+) -> Union[FixedLenFeature, VarLenFeature]:
+    if fixed_len:
+        return tf.io.FixedLenFeature([], dtype=tf.string)
+    return tf.io.VarLenFeature(dtype=tf.string)
